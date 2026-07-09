e7db4812683a8999fea033a58b47f91ebde2b68d
AkshayaRatha <akshayaratha02@gmail.com>
2026-07-09 09:16:07 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:16:01 AM
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int start = 0;
+        int end = s.length()-1;
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
