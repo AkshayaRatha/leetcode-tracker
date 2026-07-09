8fc46b72463d22ca8ecfb4ca7de4970dcb03796c
AkshayaRatha <akshayaratha02@gmail.com>
2026-07-09 09:17:11 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:17:05 AM
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (map.containsValue(c)) {
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                if (stack.isEmpty() || map.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();        
+    }
+}
