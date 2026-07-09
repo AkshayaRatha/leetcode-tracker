becf069082c0ec6e4d23859b7574135d1ae6278e
AkshayaRatha <akshayaratha02@gmail.com>
2026-07-09 09:16:58 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:16:52 AM
+public class Solution {
+	public int[] searchRange(int[] A, int target) {
+		int start = Solution.firstGreaterEqual(A, target);
+		if (start == A.length || A[start] != target) {
+			return new int[]{-1, -1};
+		}
+		return new int[]{start, Solution.firstGreaterEqual(A, target + 1) - 1};
+	}private static int firstGreaterEqual(int[] A, int target) {
+		int low = 0, high = A.length;
+		while (low < high) {
+			int mid = low + ((high - low) >> 1);
+			if (A[mid] < target) {
+				low = mid + 1;
+			} else {
+                high = mid;
+			}
+		}
+		return low;
+	}
+}
