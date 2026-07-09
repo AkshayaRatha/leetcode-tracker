c17bef4d97d47270b1a723c615bb05e0f51105ec
AkshayaRatha <akshayaratha02@gmail.com>
2026-07-09 09:14:03 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:13:57 AM
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+
+        int[] map = new int[2001] ;
+
+        for(int num : arr)
+        {
+            map[num + 1000] ++ ;
+        }
+
+        if(map[1000] > 1) return true ;
+
+        for(int num : arr)
+        {
+            if(num == 0) continue ;
+
+            if(num % 2 == 0 && map[num/2 + 1000] > 0 ) return true ;
+        }
+
+        return false ;
+        
+    }
+}
