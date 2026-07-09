59d547df6a91ed75bdd5c459ceb59fb93c19f279
AkshayaRatha <akshayaratha02@gmail.com>
2026-07-09 09:15:14 +0530

    Create 322-CoinChange

---
+++ 322-CoinChange/322-CoinChange.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:15:08 AM
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int arr[] = new int[amount + 1];
+        for(int i = 0; i <= amount; i++){
+            arr[i] = amount + 1;
+        }
+
+        arr[0] = 0;
+
+        for(int coin : coins){
+            for(int i = coin; i <= amount; i++){
+                arr[i] = Math.min(arr[i], 1 + arr[i - coin]);
+            }
+        }
+
+        if(arr[amount] > amount){
+            return - 1;
+        }
+        else{
+            return arr[amount];
+        }
+    }
+}
