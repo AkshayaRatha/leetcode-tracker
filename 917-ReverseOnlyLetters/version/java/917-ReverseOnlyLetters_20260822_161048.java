9f16acd517bfd7d742dddd07c127731c0d3d177f
AkshayaRatha <akshayaratha02@gmail.com>
2026-08-22 16:10:49 +0530

    Create 917-ReverseOnlyLetters

---
+++ 917-ReverseOnlyLetters/version/java/917-ReverseOnlyLetters_20260822_161048.java
@@ -0,0 +1,20 @@
+// Last updated: 8/22/2026, 4:10:48 PM
+1class Solution {
+2    public String reverseOnlyLetters(String s) {
+3        char[] ch = s.toCharArray();
+4        StringBuilder letters = new StringBuilder();
+5        for (char c : ch) {
+6            if (Character.isLetter(c)) {
+7                letters.append(c);
+8            }
+9        }
+10        letters.reverse();
+11        int index = 0;
+12        for (int i = 0; i < ch.length; i++) {
+13            if (Character.isLetter(ch[i])) {
+14                ch[i] = letters.charAt(index++);
+15            }
+16        }
+17        return new String(ch);
+18    }
+19}
